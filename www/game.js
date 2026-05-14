00ce7bbc02dda22008718c9212ca0b6f034f2a85
GuysServices <144134823+GuysServices@users.noreply.github.com>
2026-05-14 00:08:34 -0400

    Add files via upload

--- www/game.js
+++ www/game.js
@@ -40,7 +40,7 @@ const idleAmount = document.getElementById('idle-amount');
 const btnCollectIdle = document.getElementById('btn-collect-idle');
 
 // Game State
-const OWNER_IDS = ['GWHT00U4', 'U95I7XBL'];
+const OWNER_IDS = ['GWHT00U4', 'U95I7XBL', '5HF0MWUJ'];
 let gameState = 'LOGIN'; // LOGIN, MENU, PLAYING, END
 let isPaused = false;
 let currentUser = null;
